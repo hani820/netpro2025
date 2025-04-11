a69fdf203dc70d9396f50bab07bc1e024ba9a53f
23fi123 <hanikitifoto@gmail.com>
2025-04-11 11:15:48 +0900

    add hello

--- java-basic-1/src/Hello.java
+++ java-basic-1/src/Hello.java
@@ -1,5 +1,5 @@
-public class App {
+public class Hello {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! wowow");
+        System.out.println("Hello, World!");
     }
 }
